efca8b19786a227362c20da2a9c533b7ce5cabe7
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-03-31 09:08:09 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,236 @@
+let instagram = [
+  {
+    media: ['https://picsum.photos/id/1/293/293'],
+    title: ` najottalim «Najot Ta'lim» markazi «Eng yaxshi IT maktab» nominatsiyasi g'olibi bo'ldi!`,
+    desc: `O'zbekistondagi xususiy ta'lim salohiyatini yanada rivojlantirish maqsadida EduOn va Oliygoh.uz hamkorligida "Edu-Branding" tanlovi e'lon qilingan edi.
+
+    🎗 Tanlov natijalariga ko'ra, 3257 ta ovoz bilan «Najot Ta'lim» markazi «Eng yaxshi IT maktab» nominatsiyasining g'olibi deb topildi.
+    
+    Ushbu tanlovda «Najot Ta'lim» markaziga ishonch bildirib, o'z ovozini bergan insonlarga va tanlov tashkilotchilariga jamoamiz nomidan samimiy minnatdorchilik bildiramiz. 😊
+    
+    🚀 Siz tomoningizdan bildirilgan ushbu ishonchni oqlash uchun doimo harakat qilamiz.`,
+    id: 1,
+    like: 600
+  },
+
+  {
+    media: ['https://picsum.photos/id/311/293/293', '../media/Video-1.mp4'],
+    title: ` najottalim Dizayn battle g'oliblari aniqlandi`,
+    desc: `Bir necha kun oldin xabar berganimizdek, 26-mart kuni markazimizning Xadra filialida «Najot Ta'lim» va «Progress Academy» o'quvchilari o'rtasida dizayn yo'nalishi bo'yicha battle bo'lib o'tdi.
+
+    Musobaqa shartlariga ko'ra, berilgan 5 soat muddat ichida o'quvchilarga film uchun poster tayyorlash vazifasi topshirildi.
+    
+    ⚖️ O'quvchilarning ishlarini odilona baholash uchun har bir ishtirokchiga alohida ID raqam berildi va ular shu orqali o'z ishlarini ikki markaz ustozlaridan tashkil topgan hakamlarga yuborib, ular tomonidan holis baholandi.`,
+    id: 2,
+    like: 40
+  },
+
+  {
+    media: ['https://picsum.photos/id/411/293/293'],
+    title: `najottalim Tipografika bo'yicha foydali maslahatlar`,
+    desc: `Bir necha kun oldin xabar berganimizdek, 26-mart kuni markazimizning Xadra filialida «Najot Ta'lim» va «Progress Academy» o'quvchilari o'rtasida dizayn yo'nalishi bo'yicha battle bo'lib o'tdi.
+
+    Musobaqa shartlariga ko'ra, berilgan 5 soat muddat ichida o'quvchilarga film uchun poster tayyorlash vazifasi topshirildi.
+    
+    ⚖️ O'quvchilarning ishlarini odilona baholash uchun har bir ishtirokchiga alohida ID raqam berildi va ular shu orqali o'z ishlarini ikki markaz ustozlaridan tashkil topgan hakamlarga yuborib, ular tomonidan holis baholandi.`,
+    id: 3,
+    like: 100
+  },
+
+  {
+    media: ['https://picsum.photos/id/501/293/293', '../media/Video-2.mp4'],
+    title: `temurbek_adhamov @najottalim jamoasi bilan birga Samarqand viloyatiga safar qildik!`,
+    desc: `Men Najot Ta’lim jamoasiga birgaliklari uchun, ta’limni rivojlantirishda qo’shayotgan hissalari uchun va albatta bugun sayohat uchun tashakkur izhor qilaman! Boringizga shukr!`,
+    id: 4,
+    like: 400
+  },
+
+  {
+    media: ['https://picsum.photos/id/111/293/293'],
+    title: `najottalim Muhammadali Eshonqulov bilan suhbat bo'lib o'tdi`,
+    desc: `Kuni kecha markazimizning Chilonzor filiali o'quvchilar bilan to'lib-toshdi. Ya'ni hozirda ko'plab yoshlar uchun o'rnak bo'lib kelayotgan inson — Muhammadali Eshonqulov «Najot Ta'lim» markazida mehmon bo'ldi.
+
+    ✨ Mehmonimiz bilan o'quvchilarimiz o'rtasida samimiy suhbat bo'lib o'tdi. Spikerimiz yoshlarga ruhiyat, nafs tarbiyasi, muvaffaqiyat aslida nima ekanligi kabi bir qator mavzularda o'z maslahat va tavsiyalarini berib o'tdilar. Tadbir so'nggida esa ishtirokchilar bilan savol-javob bo'lib o'tdi. Ushbu suhbat barcha uchun manfaatli bo'lganligiga ishonamiz.
+    
+    Tashrif uchun o'zining qimmatli vaqtini ayamaganligi, bilim va tajribalarini yoshlar bilan bo'lishganligi uchun Muhammadali Eshonqulovga jamoamiz nomidan samimiy minnatdorchilik bildiramiz. `,
+    id: 5,
+    like: 15
+  },
+
+  {
+    media: ['https://picsum.photos/id/251/293/293'],
+    title: `najottalim Dizayn foundation bosqichi kirish imtihonining natijalarini e'lon qilamiz`,
+    desc: `Xabaringiz bor, Najot Ta'lim markazida 5-mart sanasidan boshlab, haftaning har shanba kuni dizayn sohasining bo'lajak vakillarini saralab olish uchun dizayn foundation bosqichi imtihonlari bo'lib o'tdi. Jumladan, bugun ham imtihonlar tashkil etildi.
+
+    🚀 Kirish imtihonlarida o'quvchilarning grafik dizayn, mantiq va ingliz tili bo'yicha tuzilgan savollarga bergan javoblari bir necha bosqichli tekshiruvlardan o'tkazildi. Dizayn foundation bosqichi uchun ushbu davrgacha umumiy hisobda 577 nafar nomzodlar ro'yxatdan o'tgan bo'lib, ularning 228 nafari kirish imtihonlarida qatnashishdi. Ulardan 151 nafari esa o'qish uchun qabul qilindi.
+    
+    📌 Kirish imtihonlaridan muvaffaqiyatli o'tgan barcha o'quvchilar 2-aprel kuniga qadar markazimiz bilan shartnoma tuzishlari talab etiladi.
+    
+    Dizayn sohasiga ilk qadamlarini muvaffaqiyatli tashlagan barcha o'quvchilarimizga keyingi faoliyatlarida ham omad va zafarlar yor bo'lishiga tilakdoshmiz. Ilm olish va rivojlanishda davom eting!  `,
+    id: 6,
+    like: 152
+  },
+
+]
+
+
+
+const MainList = document.getElementById('cardList');
+const cardItem = document.querySelector('card-item');
+
+
+instagram.forEach((item,index) =>{
+
+
+     let li = document.createElement('li');
+     li.className = 'card-item col-4';
+     li.setAttribute("data-bs-toggle","modal")
+     li.setAttribute("data-bs-target","#staticBackdrop")
+     li.innerHTML = `
+     <div class="img-wrapper">
+     <img class="card-img" src="${item.media[0]}" alt="img">
+     </div>
+     <div class="img-color">
+     <div class="d-flex justify-content-center icon-wrapper">
+       <i class='bx bxs-heart'></i>
+       <span class="me-4 ms-2  card-info">${item.like}</span>
+       <i class='bx bxs-message-rounded bx-flip-horizontal' ></i>
+       <span class="ms-2 card-info">13</span>
+     </div>
+     `;
+     let div = document.createElement('div');
+     div.innerHTML = `
+     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <button type="button" class="modal-button" data-bs-dismiss="modal" aria-label="Close">
+              <i class='bx bx-x bx-flip-vertical'></i>
+            </button>
+            <div class="modal-body">
+              <div class="d-flex">
+                <img class="card-img modal-img" src="${item.media[0]}" alt="img">
+                <div class="p-3 w-100">
+                  <div class="d-flex align-items-center justify-content-between modal-head">
+                    <div class="d-flex align-items-center">
+                      <img class="icon-img" src="media/najot-talim-icon.jpg" alt="najot" width="32" height="32">
+                      <p class="ms-3 p-0 m-0 najot-text">najottalim <span class="dot"></span> </p>
+                      <a class="ms-2 p-0" href="#">Follow</a>
+                    </div>
+                    <button><i class='bx bx-dots-horizontal-rounded bx-flip-vertical'></i></button>
+                  </div>
+                  <div class="modal-desc">
+                    <div class="mt-4 d-flex">
+                      <img class="icon-img" src="media/najot-talim-icon.jpg" alt="najot" width="32" height="32">
+                      <p class="ms-3 card-text p-0 m-0"><span class="najot-text">najottalim</span>${item.title}</p>
+                    </div>
+                    <p class="ps-5 mt-2 card-text">
+                     ${item.desc}
+                    </p>
+                    <div class="d-flex flex-wrap align-items-center">
+                      <img class="icon-img" src="media/najot-talim-icon.jpg" alt="najot" width="32" height="32">
+                      <p class="ms-3 card-text p-0 m-0"><span class="najot-text">najottalim</span></p>
+                      <a class="ms-1 card-text" href="#">#najottalim</a>
+                      <a class="ms-1 card-text" href="#">#najot</a>
+                      <a class="ms-1 card-text" href="#">#It</a>
+                      <a class="ms-1 card-text" href="#">#dizayn</a>
+                      <div class="hash-tag">
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#dizayn</a>
+                        <a class="ms-1 card-text" href="#">#najot</a>
+                        <a class="ms-1 card-text" href="#">#najot</a>
+                        <a class="ms-1 card-text" href="#">#najot</a>
+                        <a class="ms-1 card-text" href="#">#najot</a>
+                        <a class="ms-1 card-text" href="#">#najot</a>
+                        <a class="ms-1 card-text" href="#">#najot</a>
+                        <a class="ms-1 card-text" href="#">#najot</a>
+                        <a class="ms-1 card-text" href="#">#najot</a>
+                      </div>
+                    </div>
+                    <div class="d-flex align-items-center mb-3">
+                      <img class="icon-img" src="https://picsum.photos/id/135/32/32" alt="img">
+                      <p class="ms-3 card-text p-0 m-0"><span class="najot-text me-2">giyasov_abdullokh</span>Super!!!</p>
+                    </div>
+                    <div class="d-flex align-items-center mb-3">
+                      <img class="icon-img" src="https://picsum.photos/id/235/32/32" alt="img">
+                      <p class="ms-3 card-text p-0 m-0"><span class="najot-text me-2">john_smith</span>Amazing</p>
+                    </div>
+                    <div class="d-flex align-items-center mb-3">
+                    <img class="icon-img" src="https://picsum.photos/id/178/32/32" alt="img">
+                    <p class="ms-3 card-text p-0 m-0"><span class="najot-text me-2">will_smith</span>Interesting video</p>
+                  </div>
+                    <div class="d-flex align-items-center">
+                    <img class="icon-img" src="https://picsum.photos/id/735/32/32" alt="img">
+                    <p class="ms-3 card-text p-0 m-0"><span class="najot-text me-2">jack_nick</span>......</p>
+                  </div>
+                  </div>
+                  <div class="mt-3 d-flex justify-content-between">
+                    <div>
+                      <button><i class='bx  comment-icons bx-heart'></i></button>
+                      <button><i class='bx ms-2 comment-icons bx-message-rounded bx-flip-horizontal'></i></button>
+                      <button><i class='bx ms-2 comment-icons bxl-telegram'></i></button>
+                    </div>
+                    <button><i class='bx comment-icons bx-bookmark'></i></button>
+                  </div>
+                  <p class="najot-text m-0 mb-1 mt-2">${item.like} likes</p>
+                  <p class="hour-text">8 hours ago</p>
+                  <div class="d-flex w-100 align-items-center justify-content-between">
+                    <div>
+                      <i class='bx comment-icons bx-smile'></i>
+                      <input class="ms-3 comment-input" type="text" name="comment" placeholder="Add a comment...">
+                    </div>
+                    <button class="post-btn">Post</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>  
+     `;
+     MainList.appendChild(li);
+     MainList.appendChild(div);
+   
+     let med = item.media;
+     med.forEach(el =>{
+       if (el.includes('mp4')){
+         let heart = li.querySelector('.bxs-heart');
+         heart.className = 'bx bx-play bx-flip-vertical';
+         let span = document.createElement('span');
+         span.className = 'icon-span';
+         span.innerHTML = `<i class='bx bx-play bx-flip-vertical' ></i>`;
+         li.appendChild(span);
+       }
+     })
+
+})
+
+
+let like = document.querySelectorAll('.bx-heart');
+
+like.forEach(item =>{
+  item.addEventListener('click', (e)=>{
+    e.target.className = 'bx bxs-heart';
+    e.target.style.fontSize = '27px';
+    e.target.style.color = 'red';
+  })
+})
+
+
+const comment = document.querySelector('.comment-input');
+const post = document.querySelector('.post-btn');
+
+comment.addEventListener('keyup', ()=>{
+  let val = comment.value;
+  post.style.color = '#0A99F6';
+  if (val == ''){
+    post.style.color = '#0a98f65b';
+  }
+})
